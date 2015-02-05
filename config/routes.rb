9349986a67a3271e6618ef3d865dabe45c76fc5a
althe1 <alkirakosian@gmail.com>
2015-02-04 19:33:19 -0800

    Added Comments Model and Controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :posts
-
   resources :tags
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150201004321) do
+ActiveRecord::Schema.define(version: 20150205031201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "votes_score"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "post_tag_weights", force: :cascade do |t|
     t.integer  "weight"
