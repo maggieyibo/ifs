3673025dffbbf6370bebf279d3abe1170571978a
althe1 <alkirakosian@gmail.com>
2015-02-05 17:41:24 -0800

    Added Search method

--- app/controllers/ifs_controller.rb
+++ app/controllers/ifs_controller.rb
@@ -5,8 +5,20 @@ class IfsController < ApplicationController
 	end
 
   def search
-    @posts = Post.all
-    render json: @posts, each_serializer: PostSerializer
+    post_weights_array = []
+
+    search_weight = {'1': 40, '2': 20, '3': 35, '4': 5}
+    posts = Post.all
+
+    posts.each do |p|
+      score = 0
+      p.post_tag_weights.each do |ptw|
+        score += ptw.weight * search_weight[:"#{ptw.tag_id}"]
+      end
+      post_weights_array << {p.id: score}
+    end
+
+    render json: posts, each_serializer: PostSerializer
   end
 
 end
