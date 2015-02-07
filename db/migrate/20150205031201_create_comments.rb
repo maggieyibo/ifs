f278322d9a7bd9448bbffd46dfc2d542e2ed9a6e
focomoso <jkelly@focomoso.com>
2015-02-07 14:35:04 -0800

    fixing db migrate issues

---
+++ db/migrate/20150205031201_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration   
+  def change   
+    create_table :comments do |t|    
+      t.string :title    
+      t.string :body   
+      t.integer :votes_score   
+   
+      t.timestamps null: false   
+    end    
+  end    
+end
